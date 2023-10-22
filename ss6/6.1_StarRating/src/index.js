30cd67192fed45b64bbd901cdf364e6c8274b20d
yo16 <xxyo1xx@gmail.com>
2023-10-22 16:57:46 +0900

    お試し環境構築

---
+++ ss6/6.1_StarRating/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+
+import StarRating from "./StarRating";
+
+render(<StarRating />, document.getElementById("root"));
+
---
+++ ss6/6.1_StarRating3/star-rating-app/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import StarRating from "./StarRating";
+
+render(<StarRating />, document.getElementById("root"));
