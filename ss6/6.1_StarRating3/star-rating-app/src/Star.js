ebd64962ff88d283fcddb7caa716b45b48ac6fff
yo16 <xxyo1xx@gmail.com>
2023-10-22 17:14:21 +0900

    動くまで

---
+++ ss6/6.1_StarRating3/star-rating-app/src/Star.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const Star = ({selected = false, onSelect = f => f }) => (
+    <FaStar color={selected ? "red": "gray"} onClick={onSelect} />
+);
+
+export default Star;
+
